9e5d0b10df2d0781cb157443dafab394a954e505
Ines Armada Bras <ines.armada.bras@gmail.com>
2016-11-05 23:39:42 +0000

    final

---
+++ README.md
@@ -0,0 +1,73 @@
+# Neighborhood Map Project
+
+The goal of this project is to develop a single-page application featuring a map of a neighborhood (In my case, Lisbon & Margem Sul)
+
+## What I'll see?
+* This project is built using Google Maps API, Google Street View Image API, Yelp API and knockoutJS framework. Features Include:
+* A full-screen map to page using the Google Maps API.
+* A Street View Image of the location
+* Map markers identifying a number of favorite locations in Lisbon & Margem Sul
+* Images of the locations, from Google Images
+* Reviews from Yelp
+* List view of the identified locations.
+
+## How to run?
+
+Desktop:
+* Open [inesarmadabras.github.io/nanodegree-neighborhood-map/](https://inesarmadabras.github.io/nanodegree-neighborhood-map/)
+or
+* Download the repo. Just launch index.html.
+* Use the filter box to filter list items.
+* Click a list item to move to the corresponding map marker and view more info.
+
+Mobile:
+* Open [inesarmadabras.github.io/Neighborhood-Map-Project/](https://inesarmadabras.github.io/nanodegree-neighborhood-map/)
+* Click on the button at the left top corner to see the menu (search and list of localizations)
+* Click a list item to move to the corresponding map marker and view more info
+
+
+
+# Resubmission
+
+Note:
+Due to various problems, I had to big changes in a short time:
+
+* Due to many problems, app.js was redone
+* All the locations are in the file local.min.js
+* I stop using Material Framework. Now I use only Bootstrap because I'm more comfortable with it.
+* Due to problems with the Flickr API now use the Yelp API, because I already knew how to implement it.
+
+
+### index.html - Sugestions
+(1) Added theme-color
+        `<meta name="theme-color" content="#0066FF">`
+
+(2) Added favicon
+        `<link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">`
+        `<link rel="icon" href="/favicon.ico" type="image/x-icon">`
+
+(3) App is usable on small screen devices. Now you can see a menu button at the corner (only at mobile view).
+    *Update: New button,mobile menu and mobile search bar.*
+
+(4) More HTML semantic tags.Indentation.
+
+(5) All scripts are now at the bottom of the HTML body for Page Speed Optimization
+
+(7) Moved GoogleMaps API request from app.js to index.html.
+
+### index.html - Required
+(6) async atribute removed.
+
+### app.js - redone!
+* single-quotes (') over double-quotes (")
+
+* added new locations.Locations are now at the file `local.js`
+
+* var style` is now at the top of the file
+
+* map function is now inside of the ViewModel()
+
+### app.js - Required
+All locations show  images.
+All locations show a Yelp review.
+Filter Search now filter the markers too (when you press search)
---
+++ dist/js/app.js
@@ -0,0 +1,287 @@
+//Resubmission -  single-quotes (') over double-quotes (")
+//Resubmission -  var style is now at the top of the file
+//Resubmission -  file redone
+
+// Initialize the map
+var map;
+
+//The style of the map.
+var style = [ {
+    elementType: "geometry",
+    stylers: [ {
+        color: "#242f3e"
+    } ]
+}, {
+    elementType: "labels.text.stroke",
+    stylers: [ {
+        color: "#242f3e"
+    } ]
+}, {
+    elementType: "labels.text.fill",
+    stylers: [ {
+        color: "746855"
+    } ]
+}, {
+    featureType: "administrative.locality",
+    elementType: "labels.text.fill",
+    stylers: [ {
+        color: "#d59563"
+    } ]
+}, {
+    featureType: "poi",
+    elementType: "labels.text.fill",
+    stylers: [ {
+        color: "#d59563"
+    } ]
+}, {
+    featureType: "poi.park",
+    elementType: "geometry",
+    stylers: [ {
+        color: "#263c3f"
+    } ]
+}, {
+    featureType: "poi.park",
+    elementType: "labels.text.fill",
+    stylers: [ {
+        color: "#6b9a76"
+    } ]
+}, {
+    featureType: "road",
+    elementType: "geometry",
+    stylers: [ {
+        color: "#38414e"
+    } ]
+}, {
+    featureType: "road",
+    elementType: "geometry.stroke",
+    stylers: [ {
+        color: "#212a37"
+    } ]
+}, {
+    featureType: "road",
+    elementType: "labels.text.fill",
+    stylers: [ {
+        color: "#9ca5b3"
+    } ]
+}, {
+    featureType: "road.highway",
+    elementType: "geometry",
+    stylers: [ {
+        color: "#746855"
+    } ]
+}, {
+    featureType: "road.highway",
+    elementType: "geometry.stroke",
+    stylers: [ {
+        color: "#1f2835"
+    } ]
+}, {
+    featureType: "road.highway",
+    elementType: "labels.text.fill",
+    stylers: [ {
+        color: "#f3d19c"
+    } ]
+}, {
+    featureType: "transit",
+    elementType: "geometry",
+    stylers: [ {
+        color: "#2f3948"
+    } ]
+}, {
+    featureType: "transit.station",
+    elementType: "labels.text.fill",
+    stylers: [ {
+        color: "#d59563"
+    } ]
+}, {
+    featureType: "water",
+    elementType: "geometry",
+    stylers: [ {
+        color: "#17263c"
+    } ]
+}, {
+    featureType: "water",
+    elementType: "labels.text.fill",
+    stylers: [ {
+        color: "#515c6d"
+    } ]
+}, {
+    featureType: "water",
+    elementType: "labels.text.stroke",
+    stylers: [ {
+        color: "#17263c"
+    } ]
+} ];
+
+
+// Initialize the default infoWindow
+var infoWindow = new google.maps.InfoWindow({
+    // default content
+    content: '<div><h4 id="place-name"></h4><p id="place-address"></p><p id="yelp"></p></div>'
+});
+
+// now initialize map function is inside the ViewModel
+var ViewModel = function() {
+    "use strict";
+    var self = this;
+    self.placeList = ko.observableArray([]);
+    self.filteredPlaceList = ko.observableArray([]);
+
+    self.initialize = function() {
+        var mapCanvas = document.getElementById("google-map");
+        var cenLatLng = new google.maps.LatLng(38.6766764, -9.184167);
+        var mapOptions = {
+            center: cenLatLng,
+            zoom: 13,
+            styles: style,
+            disableDefaultUI: false
+        };
+        map = new google.maps.Map(mapCanvas, mapOptions);
+    };
+    // Create the list of place locations from the model
+    self.buildPlaceLocations = function() {
+        placeLocations.forEach(function(brewItem) {
+            self.placeList.push(new Place(brewItem));
+        });
+    };
+    // Set up an event listener for clicks for each place
+    self.setPlaceClickFunctions = function() {
+        self.placeList().forEach(function(place) {
+            google.maps.event.addListener(place.marker(), "click", function() {
+                self.placeClick(place);
+            });
+        });
+    };
+    // Function to handle clicking on a place (either in list or marker)
+    self.placeClick = function(place) {
+        // Set the content of the infoWindow
+        var infoContent = '<div><h4 id="place-name">' + place.name() + "</h4>" + '<h5 id="place-address">' + place.address() + "</h5>" + '<h6 id="place-type">' + place.type() + "</h6>" + '<p id="text">Rating on <a id="yelp-url">yelp</a>: ' + '<img id="yelp"></p></div><br>' + '<img class="images" src= http://maps.googleapis.com/maps/api/streetview?size=200x100&location=' + place.lat() + "," + place.lng() + ">";
+        infoWindow.setContent(infoContent);
+        self.getYelpData(place);
+        // Make the clicked on place the center of the map
+        map.panTo(new google.maps.LatLng(place.lat(), place.lng()));
+        // Open the infoWindow at the marker location
+        infoWindow.open(map, place.marker());
+        // Current place marker bounces once when clicked
+        self.toggleBounce(place);
+    };
+    // Animate marker on click
+    // ref: https://developers.google.com/maps/documentation/javascript/examples/marker-animations
+    self.toggleBounce = function(place) {
+        place.marker().setAnimation(google.maps.Animation.BOUNCE);
+        setTimeout(function() {
+            place.marker().setAnimation(null);
+        }, 1500);
+    };
+    // filter
+    self.filterPlaces = function() {
+        // Set the filtered place list to an empty array
+        self.filteredPlaceList([]);
+        // Get the search string and the length of the original place list
+        var searchString = $("#search-str").val().toLowerCase();
+        var len = self.placeList().length;
+        // Loop through each place in the place list
+        for (var i = 0; i < len; i++) {
+            // Get the current place name & type
+            var placeName = self.placeList()[i].name().toLowerCase();
+            var placeNeighborhood = self.placeList()[i].type().toLowerCase();
+            // If the name or type match the search string,
+            // add the place to the filtered place list
+            if (placeName.indexOf(searchString) > -1 || placeNeighborhood.indexOf(searchString) > -1) {
+                self.filteredPlaceList.push(self.placeList()[i]);
+                // Set the map property of the marker to the map
+                self.placeList()[i].marker().setMap(map);
+            } else {
+                // Set the map property of the marker to null so it won't be visible
+                self.placeList()[i].marker().setMap(null);
+            }
+        }
+    };
+    self.getYelpData = function(place) {
+        // Uses the oauth-signature package installed with bower per https://github.com/bettiolo/oauth-signature-js
+        // Use the GET method for the request
+        var httpMethod = "GET";
+        // Yelp API request url
+        var yelpURL = "http://api.yelp.com/v2/search/";
+        // nonce generator
+        // function credit of: https://blog.nraboy.com/2015/03/create-a-random-nonce-string-using-javascript/
+        var nonce = function(length) {
+            var text = "";
+            var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+            for (var i = 0; i < length; i++) {
+                text += possible.charAt(Math.floor(Math.random() * possible.length));
+            }
+            return text;
+        };
+        // Set required parameters for authentication & search
+        var parameters = {
+            oauth_consumer_key: "S46AQ1iwQtvxw_D1wQLHZA",
+            oauth_token: "TO9rPx1abdPe3lllR5Wo3WFrvz8CV9vw",
+            oauth_nonce: nonce(20),
+            oauth_timestamp: Math.floor(Date.now() / 1e3),
+            oauth_signature_method: "HMAC-SHA1",
+            oauth_version: "1.0",
+            callback: "cb",
+            term: place.name(),
+            location: "Portugal",
+            limit: 1
+        };
+        // Set other API parameters
+        var consumerSecret = "8hqIHpplfRBLzs6YOqLZFfkx7jg";
+        var tokenSecret = "evb3bjTox8RNlfZ5Ma74hqJjZWo";
+        // generates a RFC 3986 encoded, BASE64 encoded HMAC-SHA1 hash
+        var signature = oauthSignature.generate(httpMethod, yelpURL, parameters, consumerSecret, tokenSecret);
+        // Add signature to list of parameters
+        parameters.oauth_signature = signature;
+        // Set up the ajax settings
+        var ajaxSettings = {
+            url: yelpURL,
+            data: parameters,
+            cache: true,
+            dataType: "jsonp",
+            success: function(response) {
+                // Update the infoWindow to display the yelp rating image
+                $("#yelp").attr("src", response.businesses[0].rating_img_url);
+                $("#yelp-url").attr("href", response.businesses[0].url);
+            },
+            error: function() {
+                $("#text").html("Data could not be retrieved from yelp.");
+            }
+        };
+        // Send off the ajaz request to Yelp
+        $.ajax(ajaxSettings);
+    };
+    // Add the listener for loading the page
+    google.maps.event.addDomListener(window, "load", function() {
+        self.initialize();
+        self.buildPlaceLocations();
+        self.setPlaceClickFunctions();
+        self.filteredPlaceList(self.placeList());
+    });
+};
+
+var Place = function(data) {
+    "use strict";
+    // Set all the properties as knockout observables
+    var marker;
+    this.name = ko.observable(data.name);
+    this.lat = ko.observable(data.lat);
+    this.lng = ko.observable(data.lng);
+    this.address = ko.observable(data.address);
+    this.type = ko.observable(data.type);
+    // Google Maps Marker for this location
+    marker = new google.maps.Marker({
+        position: new google.maps.LatLng(this.lat(), this.lng()),
+        map: map,
+        title: this.name()
+    });
+    // Set the marker as a knockout observable
+    this.marker = ko.observable(marker);
+};
+
+ko.applyBindings(new ViewModel());
+
+// If google maps is not working, throw and error message.
+function googleError() {
+    window.alert("Google Maps request error");
+}
